b2464174fd614d1d4da23e7fd15f93177a474847
Sergey <40739781+sergey-ishchenko@users.noreply.github.com>
2025-03-16 00:31:07 +0200

    Update TestGithubConsoleApp/Program.cs

--- TestGithubConsoleApp/Program.cs
+++ TestGithubConsoleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, My big World!");
+﻿Console.WriteLine("First Line");
+Console.WriteLine("Hello, My small World!");
+Console.WriteLine("Hello, Ukraine!");
